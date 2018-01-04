0002d97c935c059fbd20a3cf929daa458b3f3307
Kamal2511 <31500911+Kamal2511@users.noreply.github.com>
2018-01-05 00:54:54 +0530

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+import tkinter.messagebox
+tk = Tk()
+tk.title("Tic Tac Toe")
+
+bclick = True
+
+def checker(buttons):
+    global bclick
+    if buttons["text"] == " " and bclick==True:
+        buttons["text"] = "X"
+        bclick = False
+    elif buttons["text"] == " " and bclick== False:
+        buttons["text"] = "O"
+        bclick =True
+    elif (button1["text"] == "X" and button2["text"] == "X" and button3["text"]=="X")or (button4["text"] == "X" and button5["text"] == "X" and button6["text"]=="X")or (button7["text"] == "X" and button8["text"] == "X" and button9["text"]=="X")or(button1["text"] == "X" and button4["text"] == "X" and button7["text"]=="X") or(button2["text"] == "X" and button5["text"] == "X" and button8["text"]=="X") or(button3["text"] == "X" and button6["text"] == "X" and button9["text"]=="X") or(button1["text"] == "X" and button5["text"] == "X" and button9["text"]=="X") or(button3["text"] == "X" and button5["text"] == "X" and button7["text"]=="X"):
+        tkinter.messagebox.showinfo("Winner X", "X have won a Game")
+
+    elif (button1["text"] == "O" and button2["text"] == "O" and button3["text"]=="0")or (button4["text"] == "O" and button5["text"] == "O" and button6["text"]=="0")or (button7["text"] == "O" and button8["text"] == "O" and button9["text"]=="0")or(button1["text"] == "0" and button4["text"] == "O" and button7["text"]=="0") or(button2["text"] == "O" and button5["text"] == "O" and button8["text"]=="0") or(button3["text"] == "O" and button6["text"] == "O" and button9["text"]=="0") or(button1["text"] == "O" and button5["text"] == "O" and button9["text"]=="0") or(button3["text"] == "O" and button5["text"] == "O" and button7["text"]=="0"):
+        tkinter.messagebox.showinfo("Winner O", "O have won a Game")
+    else:
+        tkinter.messagebox.showinfo("Draw","tied..!")
+buttons = StringVar()
+button1 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button1))
+button1.grid(row=1 , column=0,sticky = S+N+E+W)
+button2 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button2))
+button2.grid(row=1 , column=1,sticky = S+N+E+W)
+button3 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button3))
+button3.grid(row=1 , column=2,sticky = S+N+E+W)
+button4 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button4))
+button4.grid(row=2 , column=0,sticky = S+N+E+W)
+button5 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button5))
+button5.grid(row=2 , column=1,sticky = S+N+E+W)
+button6 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button6))
+button6.grid(row=2 , column=2,sticky = S+N+E+W)
+button7 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button7))
+button7.grid(row=3 , column=0,sticky = S+N+E+W)
+button8 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button8))
+button8.grid(row=3 , column=1,sticky = S+N+E+W)
+button9 = Button(tk,text= " ",font=("Times 30 bold"), height = 4, width = 8, command=lambda:checker(button9))
+button9.grid(row=3 , column=2,sticky = S+N+E+W)
+
+tk.mainloop()
